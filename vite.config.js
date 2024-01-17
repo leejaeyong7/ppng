6490f6079e35f1c3afc44aff8dad0435c4132a72
Jae Yong Lee <leejaeyong7@gmail.com>
2024-01-16 18:32:00 -0600

    updating presets

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,35 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/eqff/',
   // omit
   server: {
     watch: {
         ignored: ['**/public/**']
     }
-}
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        qff: resolve(__dirname, 'qff.html'),
+        blended_mvs_1: resolve(__dirname, 'presets', 'blended_mvs_1.html'),
+        blended_mvs_2: resolve(__dirname, 'presets', 'blended_mvs_2.html'),
+        blended_mvs_3: resolve(__dirname, 'presets', 'blended_mvs_3.html'),
+        mipnerf_1: resolve(__dirname, 'presets', 'mipnerf_1.html'),
+        mipnerf_2: resolve(__dirname, 'presets', 'mipnerf_2.html'),
+        mipnerf_3: resolve(__dirname, 'presets', 'mipnerf_3.html'),
+        nerf_1: resolve(__dirname, 'presets', 'nerf_1.html'),
+        nerf_2: resolve(__dirname, 'presets', 'nerf_2.html'),
+        nerf_3: resolve(__dirname, 'presets', 'nerf_3.html'),
+        nsvf_1: resolve(__dirname, 'presets', 'nsvf_1.html'),
+        nsvf_2: resolve(__dirname, 'presets', 'nsvf_2.html'),
+        nsvf_3: resolve(__dirname, 'presets', 'nsvf_3.html'),
+        tnt_1: resolve(__dirname, 'presets', 'tnt_1.html'),
+        tnt_2: resolve(__dirname, 'presets', 'tnt_2.html'),
+        tnt_3: resolve(__dirname, 'presets', 'tnt_3.html'),
+      },
+    },
+  }
 })
